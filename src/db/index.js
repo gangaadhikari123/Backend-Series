09c3fe9bc52eb6253f091618895517dc40ca730e
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-23 14:01:38 +0545

    custom api and error handling

--- src/db/index.js
+++ src/db/index.js
@@ -4,13 +4,14 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async() => {
     try{
-         const connectionInstance = await mongoose.connect(`${process.env.
-            MONGODB_URI}/${DB_NAME}`)
+         const connectionInstance = await mongoose.connect
+         (`${process.env.MONGODB_URI}/${DB_NAME}`)
             console.log(`\n Mongodb connected !!  DB HOSt:
                 ${connectionInstance.connection.host}`);
 
 
-    } catch(error){console.log("MongDB connection error", error)
+    } catch(error){
+        console.log("MongDB connection error", error)
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at poer: 
+            ${process.env.PORT}`);
+
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed!!!", err);
+
+})
 
 
 
