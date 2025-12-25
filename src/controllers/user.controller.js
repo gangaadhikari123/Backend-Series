2fddc0f1db4ac63ca14c52a15e585a681be64816
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-25 18:04:44 +0545

    logic building

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import { user } from "../models/user.model.js";
 
+import {uploadOnCloudinary} from "../utils/cloudnary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
 const {fullname,email,username,password} = req.body
@@ -21,7 +25,7 @@ console.log("email",email);
     throw new ApiError(400, "All fields are required")
   }
 
-   const existedUser = User.findOne({
+   const existedUser = user.findOne({
     $or: [{username}, {email}]
   })
 
@@ -29,14 +33,60 @@ if (existedUser){
   throw new ApiError(409, "User with email or username already exist")
 }
 
-req.files?.avatar[0]?.path
+ const avatarLocalPath = req.files?.avatar[0]?.path;
+
+ const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
 
   // check if user already exit: username, email
+
+  if(!avatarLocalPath){
+    throw new ApiError(400,"Avatar cover image not uploaded")
+
+  }
+
   //check if files exist; images and avatar
+
+
   //upload them to cloudnairy
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!avatar){
+    throw new ApiError(400, "Avatar required")
+  }
+
+
+  const user = await user.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+})
   // create user object-create entry in db
+
+   const createdUser = await user.findById(user._id).select(
+    "-password -refreshToken"
+   )
+
+   if (!createdUser){
+    throw new ApiError(500, "Something went wrong while registing user ")
+
+   }
+
+   return res.status(201).json(
+    
+    new ApiResponse(200, createdUser, "User register successfully")
+  )
   //remove password and refresh token field from response
   // check foe user creation
+
+
+
+
   //return response
 
 
