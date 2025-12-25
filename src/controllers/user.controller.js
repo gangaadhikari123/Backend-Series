6690e1b0ddfa5f85e9726ac7fc1585b69fff5048
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-25 17:25:30 +0545

    user data taken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
+const {fullname,email,username,password} = req.body
+console.log("email",email);
+
+
+  //validation - n ot empty
+  // check if user already exit: username, email
+  //check if files exist; images and avatar
+  //upload them to cloudnairy
+  // create user object-create entry in db
+  //remove password and refresh token field from response
+  // check foe user creation
+  //return response
+
+
+
+
   
 });
 
