53d9d35e0018e58a62d9e4dc3208eb3f20851f43
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-25 17:42:04 +0545

    connetion between frntend and bk

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { user } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -7,6 +9,28 @@ console.log("email",email);
 
 
   //validation - n ot empty
+  // if (fullname===""){
+  //   throw new ApiError(400, "fullname is required")
+  // }
+
+  if(
+
+    [ fullname, email, username, password].some((field)=>
+    field?.trim()==="")
+  ){
+    throw new ApiError(400, "All fields are required")
+  }
+
+   const existedUser = User.findOne({
+    $or: [{username}, {email}]
+  })
+
+if (existedUser){
+  throw new ApiError(409, "User with email or username already exist")
+}
+
+req.files?.avatar[0]?.path
+
   // check if user already exit: username, email
   //check if files exist; images and avatar
   //upload them to cloudnairy
